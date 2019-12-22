f7b726622fde0a88089b31de622ea6e79051ca89
shraddhamule <shraddhaamule94@gmail.com>
2019-12-22 12:50:33 +0530

    Updates

---
+++ precache-manifest.24e9b15458b34df6eca987d3f21ce619.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5c2f5a32d41ef267c44a775ebc645fc6",
+    "url": "/RoboApp/index.html"
+  },
+  {
+    "revision": "c65bae92e895dfbc9ded",
+    "url": "/RoboApp/static/css/main.a0cfa00e.chunk.css"
+  },
+  {
+    "revision": "0422e23df8f8c945a61f",
+    "url": "/RoboApp/static/js/2.9dea136f.chunk.js"
+  },
+  {
+    "revision": "d705cb622423d72c5defbf368ca70dcc",
+    "url": "/RoboApp/static/js/2.9dea136f.chunk.js.LICENSE"
+  },
+  {
+    "revision": "c65bae92e895dfbc9ded",
+    "url": "/RoboApp/static/js/main.78b032f3.chunk.js"
+  },
+  {
+    "revision": "ea7b13ab850c7c85941a",
+    "url": "/RoboApp/static/js/runtime-main.d8d33ae9.js"
+  }
+]);
